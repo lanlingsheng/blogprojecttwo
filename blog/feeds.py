ce3730a607a136dabab47ab5d8a2ace8cf2ff215
lanlingsheng <781192559@qq.com>
2018-04-14 18:01:17 +0800

    增加 RSS 订阅

---
+++ blog/feeds.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib.syndication.views import Feed
+
+from .models import Post
+
+
+class AllPostsRssFeed(Feed):
+    title = "Django 博客教程演示项目"
+
+    link = "http://www.rubic"
+
+    description = "Django 博客教程"
+
+    def items(self):
+        return Post.objects.all()
+
+    def item_title(self, item):
+        return '[%s] %s'% (item.category, item.title)
+
+    def item_description(self, item):
+        return item.body
