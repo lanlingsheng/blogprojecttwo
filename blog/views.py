8f066d987fbd74bab99819988bb9773f27b74ee4
lanlingsheng <781192559@qq.com>
2018-04-14 16:44:47 +0800

    简化detail.html和 index.html文件内容

--- blog/views.py
+++ blog/views.py
@@ -13,6 +13,48 @@ class IndexView(ListView):
     context_object_name = 'post_list'
     paginate_by = 5
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated = context.get('is_paginated')
+        pagination_data = self.pagination_data(paginator, page, is_paginated)
+        context.update(pagination_data)
+        return context
+
+    def pagination_data(self, paginator, page, is_paginated):
+        if not is_paginated:
+            return {}
+        left_has_more = False
+        right_has_more = False
+        first = False
+        last = False
+        page_number = page.number
+        total_pages = paginator.num_pages
+        page_range = paginator.page_range
+        right = page_range[page_number:page_number + 2]
+        left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0: page_number - 1]
+        if right:
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+        if left:
+            if left:
+                if left[0] > 2:
+                    left_has_more = True
+                if left[0] > 1:
+                    first = True
+        data = {
+            'left': left,
+            'right': right,
+            'right_has_more': right_has_more,
+            'left_has_more': left_has_more,
+            'first': first,
+            'last': last,
+        }
+        return data
+
 
 class CategoryView(ListView):
     model = Post
@@ -42,7 +84,7 @@ class PostDetailView(DetailView):
         # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
         post = super(PostDetailView, self).get_object(queryset=None)
         post.body = markdown.markdown(post.body,
-                                      extensions = [
+                                      extensions=[
                                           'markdown.extensions.extra',
                                           'markdown.extensions.codehilite',
                                           'markdown.extensions.toc',
@@ -60,7 +102,6 @@ class PostDetailView(DetailView):
             'comment_list': comment_list,
         })
         return context
-
 
 
 def index(request):
